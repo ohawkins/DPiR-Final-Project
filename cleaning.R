4a6334cb260a6ecd3ecb559c155487f4bcf435e3
Olivia Hawkins <olihawkins@uiowa.edu>
2020-11-09 13:56:19 -0600

    Add files via upload

--- cleaning.R
+++ cleaning.R
@@ -3,7 +3,7 @@ library(readr)
 library(ggplot2)
 
 # National Toplines
-df <- read_csv('presidential_national_toplines_2020.csv')
+df <- read_csv('presidential_national_toplines_2020_oct28.csv')
 df$modeldate <- as.Date(df$modeldate, format = "%m/%d/%Y")
 
 df <- subset(df, select = -c(cycle, 
@@ -28,7 +28,7 @@ p <- qplot(modeldate, ec_nomajority, data = df, geom = 'point')
 
 
 # Polls
-df <- read_csv('presidential_polls_2020.csv')
+df <- read_csv('presidential_polls_2020_oct28.csv')
 df$startdate <- as.Date(df$startdate, format = "%m/%d/%Y")
 df$enddate <- as.Date(df$enddate, format = "%m/%d/%Y")
 df$candidate_name <- factor(df$candidate_name)
@@ -39,4 +39,10 @@ df$modeldate <- as.Date(df$modeldate, format = "%m/%d/%Y")
 df$state <- factor(df$state)
 df <- subset(df, select = -c(cycle, tracking))
 
-df <- read_csv('presidential_poll_averages_2020.csv')
+
+# Presidential poll averages
+df <- read_csv('presidential_poll_averages_2020_nov2.csv')
+df$state <- factor(df$state)
+df$modeldate <- as.Date(df$modeldate, format = '%m/%d/%Y')
+df$candidate_name <- factor(df$candidate_name)
+df <- subset(df, select = -cycle)
