684a933df73165d6ebd411e981a5b9d6bb594f7b
Olivia Hawkins <olihawkins@uiowa.edu>
2020-12-06 17:30:51 -0600

    Update cleaning.R

--- cleaning.R
+++ cleaning.R
@@ -1,4 +1,3 @@
-
 library(readr)
 library(ggplot2)
 library(dplyr)
@@ -7,34 +6,20 @@ library(reshape2)
 
 # Function
 qp <- function(x = '', y = '', geo = '') {
-    p <- qplot(x, y, data = df, geom = geo)
+    p <- qplot(x, y, data = df_nat_toplines, geom = geo)
     p
 }
 
 # National Toplines
 df_nat_toplines <- read_csv('presidential_national_toplines_2020_nov2.csv')
-df_nat_toplines <- subset(df_nat_toplines, select = -c(cycle, 
-                             branch, 
-                             model, 
-                             candidate_inc, 
-                             candidate_chal, 
-                             candidate_3rd,
-                             popwin_3rd,
-                             ev_3rd,
-                             ev_3rd_hi,
-                             ev_3rd_lo,
-                             national_voteshare_3rd_hi,
-                             national_voteshare_3rd_lo,
-                             ecwin_3rd,
-                             national_voteshare_3rd,
-                             simulations))
+df_nat_toplines <- subset(df_nat_toplines, select = -c(cycle, branch, model, candidate_inc, candidate_chal, candidate_3rd, popwin_3rd, ev_3rd, ev_3rd_hi, ev_3rd_lo, national_voteshare_3rd_hi, national_voteshare_3rd_lo, ecwin_3rd, national_voteshare_3rd, simulations))
 df_nat_toplines$modeldate <- as.Date(df_nat_toplines$modeldate, format = "%m/%d/%Y")
 
 p_date_ev_inc <- qplot(modeldate, ev_inc, data = df_nat_toplines, geom = 'point', color = (ev_inc_hi-ev_inc_lo))
 p_date_ev_inc <- p_date_ev_inc + ggtitle('Forecasted # of Electoral Votes for Trump') 
 p_date_ev_inc <- p_date_ev_inc + xlab('Date') 
 p_date_ev_inc <- p_date_ev_inc + ylab('Votes')
-p_date_ev_inc <- p_date_ev_chal + ylim(190, 350)
+p_date_ev_inc <- p_date_ev_inc + ylim(190, 350)
 p_date_ev_inc <- p_date_ev_inc + scale_color_gradient(name = "range", high = 'white', low = 'red')
 
 p_date_ev_chal <- qplot(modeldate, ev_chal, data = df_nat_toplines, geom = 'point', color = (ev_chal_hi-ev_chal_lo))
@@ -98,32 +83,32 @@ df_evprob$evprob_3rd <- NULL
 df_evprob$simulations <- NULL
 df_evprob$timestamp <- NULL
 df_evprob <- rename(df_evprob, "Incumbent" = "candidate_inc",
-             "Challenger" = "candidate_chal",
-             "Date" = "modeldate",
-             "Num Electoral Votes" = "total_ev", 
-             "Inc Chance of Winning EV" = "evprob_inc", 
-             "Chal Chance of Winning EV" = "evprob_chal")
-             
+                    "Challenger" = "candidate_chal",
+                    "Date" = "modeldate",
+                    "Num Electoral Votes" = "total_ev", 
+                    "Inc Chance of Winning EV" = "evprob_inc", 
+                    "Chal Chance of Winning EV" = "evprob_chal")
+
 
 df2_evprob <- df_evprob[, c(4,5,6)]
 df2_evprob <- melt(df2_evprob, id.vars = c("Num Electoral Votes"))
 
 highlight_df2 <- df2_evprob %>%
-  filter(variable == "Chal Chance of Winning EV" & `Num Electoral Votes` == 306)
+    filter(variable == "Chal Chance of Winning EV" & `Num Electoral Votes` == 306)
 highlight2_df2 <- df2_evprob %>%
-  filter(variable == "Inc Chance of Winning EV" & `Num Electoral Votes` == 232)
+    filter(variable == "Inc Chance of Winning EV" & `Num Electoral Votes` == 232)
 
 p_evprob <- ggplot(df2_evprob, aes(`Num Electoral Votes`, value, col = variable)) +
-  geom_point()
+    geom_point()
 p_evprob <- p_evprob + scale_color_brewer(palette = "Set1", name = "Candidate", labels = c("Trump", "Biden")) + xlab("Total Electoral Votes") + ylab("Probability") + geom_vline(xintercept = 270, linetype = "dashed") + ggtitle("Forecasted probability of each EC outcome by candidate") + labs(subtitle = "(as of October 28th)") + theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))
 p_evprob <- p_evprob + geom_point(data = highlight_df2, 
-                    aes(x = highlight_df2$`Num Electoral Votes`, y = highlight_df2$value),
-                    color = "black",
-                    size = 3)
+                                  aes(x = highlight_df2$`Num Electoral Votes`, y = highlight_df2$value),
+                                  color = "black",
+                                  size = 3)
 p_evprob <- p_evprob + geom_point(data = highlight2_df2,
-                    aes(x = highlight2_df2$`Num Electoral Votes`, y = highlight2_df2$value),
-                    color = "black",
-                    size = 3)
+                                  aes(x = highlight2_df2$`Num Electoral Votes`, y = highlight2_df2$value),
+                                  color = "black",
+                                  size = 3)
 p_evprob   
 #ggsave("EVProbPlot.png", plot = p_evprob, width = 6, height = 4, units = "in", dpi = 600)
 
@@ -139,21 +124,21 @@ df_ecvspop$ecwin_3rd <- NULL
 df_ecvspop$timestamp <- NULL
 df_ecvspop$modeldate <- as.Date(df_ecvspop$modeldate, format = "%m/%d/%Y")
 df_ecvspop <- rename(df_ecvspop, "Date" = "modeldate",
-             "Incumbent" = "candidate_inc",
-             "Challenger" = "candidate_chal", 
-             "LB popular vote" = "lower_bin_text",
-             "UB popular vote" = "upper_bin_text", 
-             "Trump wins EC" = "ecwin_inc",
-             "Biden wins EC" = "ecwin_chal",
-             "Nobody wins EC" = "ecwin_nomajority", 
-             "Trump EC votes" = "total_ev_inc",
-             "LB Trump EC votes" = "ev_inc_lo", 
-             "UB Trump EC votes" = "ev_inc_hi",
-             "Biden EC votes" = "total_ev_chal",
-             "LB Biden EC votes" = "ev_chal_lo",
-             "UB Biden EC votes" = "ev_chal_hi",
-             "Num sims" = "count",
-             "Total sims" = "simulations")
+                     "Incumbent" = "candidate_inc",
+                     "Challenger" = "candidate_chal", 
+                     "LB popular vote" = "lower_bin_text",
+                     "UB popular vote" = "upper_bin_text", 
+                     "Trump wins EC" = "ecwin_inc",
+                     "Biden wins EC" = "ecwin_chal",
+                     "Nobody wins EC" = "ecwin_nomajority", 
+                     "Trump EC votes" = "total_ev_inc",
+                     "LB Trump EC votes" = "ev_inc_lo", 
+                     "UB Trump EC votes" = "ev_inc_hi",
+                     "Biden EC votes" = "total_ev_chal",
+                     "LB Biden EC votes" = "ev_chal_lo",
+                     "UB Biden EC votes" = "ev_chal_hi",
+                     "Num sims" = "count",
+                     "Total sims" = "simulations")
 
 
 df_ecvspop$winner <- as.numeric(df_ecvspop$`Trump wins EC` > 0.5)
@@ -165,7 +150,7 @@ df_ecvspop$winner <- factor(df_ecvspop$winner)
 p_ecvspop <- qplot(df_ecvspop$winner, df_ecvspop$`Num sims`, data = df_ecvspop, geom = "boxplot", color = winner) + geom_jitter(alpha = I(0.1))
 p_ecvspop <- p_ecvspop + xlab("Winner") + ylab("Number of Sims")
 p_ecvspop <- p_ecvspop + scale_color_manual(values = c("Biden" = "blue",
-                                       "Trump" = "red"))
+                                                       "Trump" = "red"))
 p_ecvspop <- p_ecvspop + ggtitle("Winners per simulation", subtitle = "(as of October 28th)") + theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))
 p_ecvspop
 
